71398420938e384930556c97ae11c30df7833ca2
andersonvalentim <vectro2610@gmail.com>
2019-06-30 10:34:34 -0300

    Adicionando as atualizaçoes do dia 30 de junho de 2018

--- ProjetoTexugoScan/ProjetoTexugoScan/FlaskInit/app.py
+++ ProjetoTexugoScan/ProjetoTexugoScan/FlaskInit/app.py
@@ -12,6 +12,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template("index.html")
+@app.route('/result')
+def result():
+    return render_template("result.html")
+
 
 @app.route('/collect',methods=['POST'])
 def collect():
